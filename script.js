f2356d460424b694a272503726c9596732140bbe
Sanskark28 <sanskarkhandelwal28032006@gmail.com>
2026-02-17 17:45:16 +0530

    Refactor geolocation and report submission functions
    Updated getLocation function to use showPosition callback and added map iframe display. Enhanced submitReport function to store status in localStorage.

--- script.js
+++ script.js
@@ -1,28 +1,36 @@
-function getLocation(){
+function previewImage(event)
+{
+let image = document.getElementById("preview");
+image.src = URL.createObjectURL(event.target.files[0]);
+}
 
-if(navigator.geolocation){
+function getLocation()
+{
+if(navigator.geolocation)
+{
+navigator.geolocation.getCurrentPosition(showPosition);
+}
+}
 
-navigator.geolocation.getCurrentPosition(
-
-function(position){
-
+function showPosition(position)
+{
 let lat = position.coords.latitude;
 let lon = position.coords.longitude;
 
 document.getElementById("location").value =
 lat + ", " + lon;
 
+document.getElementById("map").innerHTML =
+'<iframe width="100%" height="200" src="https://maps.google.com/maps?q='
++ lat + ',' + lon +
+'&z=15&output=embed"></iframe>';
 }
 
-);
-
-}
-
-}
-
-function submitReport(){
+function submitReport()
+{
+localStorage.setItem("rescueStatus",
+"Request submitted. Rescuer coming.");
 
 document.getElementById("status").innerHTML =
 "Request submitted successfully";
-
 }
